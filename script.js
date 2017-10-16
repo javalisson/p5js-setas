4e2e125c7f96aa7b6f46db15c26755fed13b90b2
JavAlisson <javalisson@gmail.com>
2017-10-16 16:22:56 -0200

    Adicionada posição à seta e à função que desenha

--- script.js
+++ script.js
@@ -1,19 +1,26 @@
 /**
- * p5js Setas
+ * p5js imgSetas
  * 
  * 
  */
 
-var seta;
+var imgSeta;
+var meuAngulo = 0;
+var s = [];
 
 function preload() {
-	seta = loadImage('imagem-2.png');
+	imgSeta = loadImage('imagem-2.png');
 }
 
 // chamada no inicio do programa
 function setup() {
 	// cria o quadro, com dimensoes 900 x 400
 	createCanvas(900, 400);
+	s.push({
+		x: 0,
+		y: 0,
+		ang: 0
+	});
 }
 
 // chamada toda vez que o quadro for redesenhado
@@ -21,12 +28,15 @@ function setup() {
 function draw() {
 	// pinta o fundo de preto
 	background(255);
-	desenha_seta();
+	var i = 0;
+	desenha_imgSeta(s[i].x, s[i].y, s[i].ang);
 }
 
-function desenha_seta() {
+function desenha_imgSeta(x, y, angulo) {
 	push();
-	// ...
-	image(seta, 0, 0, 200/2, 28/2);
+	// translate(0.5*200/2, 0.5*28/2);
+	translate(x, y);
+	rotate(angulo);
+	image(imgSeta, -0.5*200/2, -0.5*28/2, 200/2, 28/2);
 	pop();
 }
